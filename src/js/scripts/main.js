0f7460958c8dd18408be4e47d3e0412462298fa0
Realrudrapro <rudradg@proton.me>
2026-09-17 17:59:41 -0400

    TEST

---
+++ src/js/scripts/main.js
@@ -0,0 +1,29 @@
+const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+const audioContext = new AudioContext();
+const source = audioContext.createMediaStreamSource(stream);
+const analyser = audioContext.createAnalyser();
+
+source.connect(analyser);
+
+const data = new Uint8Array(analyser.fftSize);
+
+function checkSound() {
+    analyser.getByteTimeDomainData(data);
+
+    let volume = 0;
+
+    for (let i = 0; i < data.length; i++) {
+        volume += Math.abs(data[i] - 128);
+    }
+
+    volume /= data.length;
+
+    if (volume > 20) {
+        console.log("LOUD SOUND!");
+        // Put whatever you want controlled here
+    }
+
+    requestAnimationFrame(checkSound);
+}
+
+checkSound();
